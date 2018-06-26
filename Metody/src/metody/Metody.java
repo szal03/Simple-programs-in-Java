437e8a871da8417574c5b9b363faa6bc739ebe12
szal03 <33811775+szal03@users.noreply.github.com>
2018-06-26 13:35:00 +0200

    methods

---
+++ Metody/src/metody/Metody.java
@@ -0,0 +1,43 @@
+
+package metody;
+
+public class Metody 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        Test a = new Test();
+        //a.wypisz();
+        a.wypisz("hakuna matatha", "jak cudownie to brzmi");
+        
+        int wynik=a.suma(30,30);
+        
+        System.out.println(wynik);
+        
+        double wynik2 = a.dzielenie(10.0, 2.0);
+        System.out.println(wynik2);
+        
+    }
+    
+}
+
+class Test
+{
+    void wypisz(String slowo, String zdanie)
+    {
+        System.out.println(slowo+" "+zdanie);
+    }
+    
+    int suma(int liczba1, int liczba2)
+    {
+        return liczba1+liczba2;
+    }
+    double dzielenie(double a, double b)
+    {
+        if(b==0)
+            return 0;
+        
+        return a/b;
+    }
+}
