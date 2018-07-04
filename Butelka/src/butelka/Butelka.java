551b217a70d5a24f28dd52ba5f3e7559a920463b
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-04 15:48:17 +0200

    task update

--- Butelka/src/butelka/Butelka.java
+++ Butelka/src/butelka/Butelka.java
@@ -5,21 +5,34 @@ package butelka;
 public class Butelka
 {
     private double ileLitrow;
-    
+    private double pojemnosc;
   
-    Butelka(double ileLitrow)
+    Butelka(double ileLitrow,double pojemnosc)
     {
         this.ileLitrow = ileLitrow;
+        this.pojemnosc = pojemnosc;
     }
     
     double getIleLitrow()
     {
         return ileLitrow;
     }
-  
-    void wlej(double ilosc)
+    
+    double getPojemnosc()
     {
-        this.ileLitrow += ilosc;
+         return pojemnosc;
+    }
+  
+    void wlej(double ilosc, double pojemnosc)
+    {
+        if(this.ileLitrow+ilosc<=pojemnosc)
+        {
+            this.ileLitrow += ilosc;
+        }
+        else
+        {
+            System.out.println("woda w butelce przeleje sie");
+        }
     }
     
     boolean wylej(double ilosc)
@@ -36,7 +49,7 @@ public class Butelka
     {
         if(this.wylej(ilosc))
         {
-        gdziePrzelac.wlej(ilosc);
+        gdziePrzelac.wlej(ilosc,pojemnosc);
         }
         else
             System.out.println("Za malo");
@@ -46,19 +59,21 @@ public class Butelka
     {
           Butelka[] butelka1 = new Butelka[3]; // null
           
-          butelka1[0] = new Butelka(5);
-          butelka1[1]=new Butelka(10);
-          butelka1[2]=new Butelka(15);
+          butelka1[0] = new Butelka(5,10);
+          butelka1[1]=new Butelka(10,20);
+          butelka1[2]=new Butelka(15,30);
           
-          butelka1[0].wlej(6);
-          butelka1[2].wylej(7);
+        //  butelka1[0].wlej(6);
+         // butelka1[2].wylej(7);
           
-          butelka1[1].przelej(4,butelka1[0]); // zgadza sie 
+          //butelka1[1].przelej(4,butelka1[0]); // zgadza sie 
           
           System.out.println(butelka1[0].getIleLitrow());
+          System.out.println(butelka1[0].getPojemnosc());
           System.out.println(butelka1[1].getIleLitrow());
+          System.out.println(butelka1[1].getPojemnosc());
           System.out.println(butelka1[2].getIleLitrow());
-        
+          System.out.println(butelka1[2].getPojemnosc());
     }
     
 }
