0905995c97b71b82eea87d481f7d42132ba1ad6e
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-09 14:44:11 +0200

    second step

--- Klasywewnetrzne/src/klasywewnetrzne/Klasywewnetrzne.java
+++ Klasywewnetrzne/src/klasywewnetrzne/Klasywewnetrzne.java
@@ -7,13 +7,24 @@ public class Klasywewnetrzne {
   
     public static void main(String[] args) {
    
+        A zewnetrzna = new A();
+        
+        A.B tmp = zewnetrzna.new B(); // do klasy zagnieżdzonej 
     }
     
 }
 class A
 {
+    A()
+    {
+        System.out.println("Z klasy zewnętrznej");
+    }
     class B
     {
+         B()
+    {
+        System.out.println("Z klasy zagnieżdzonej");
+    }
         void cos()
         {
             test = 5;
