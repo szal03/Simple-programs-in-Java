1785dbf6ac426b92081646917f536fc6f333e673
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-09 14:37:41 +0200

    first step

--- Klasywewnetrzne/src/klasywewnetrzne/Klasywewnetrzne.java
+++ Klasywewnetrzne/src/klasywewnetrzne/Klasywewnetrzne.java
@@ -10,3 +10,21 @@ public class Klasywewnetrzne {
     }
     
 }
+class A
+{
+    class B
+    {
+        void cos()
+        {
+            test = 5;
+        }
+        
+        private int tmp;
+    }
+    void cos2()
+    {
+        //tmp = 2;
+    }
+    private int test;
+    
+}
