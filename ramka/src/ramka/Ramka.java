746da3ba98c90c3a73b854f91d43b9f529ceb8f7
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-18 12:23:22 +0200

    second commit

--- ramka/src/ramka/Ramka.java
+++ ramka/src/ramka/Ramka.java
@@ -4,22 +4,42 @@ import javax.swing.*;
 import java.awt.*; // awt - abstract window toolkit
 
 
-public class Ramka 
+public class Ramka extends JFrame 
 {
     public Ramka()
     {
-      JFrame frame = new JFrame();  
-      frame.setTitle("Moja Ramka");
-      
-      
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//      frame.setDefaultCloseOperation(3); // tezz exit (to samo co wyzej ) 
-      frame.setVisible(true);
+//      JFrame frame = new JFrame();  // prez konstruktor tez mozna nadac tytul ramki
+//      frame.setTitle("Moja Ramka");
+//      
+//      frame.setSize(500,400);
+//      
+//      
+//      frame.setLocation(500, 400);
+//      
+////      frame.setBounds(0, 0, 0, 0);
+//
+//        frame.setBackground(Color.BLUE);
+//        
+//        frame.setResizable(false);
+//        
+//
+//      
+//      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+////      frame.setDefaultCloseOperation(3); // tezz exit (to samo co wyzej ) 
+//      frame.setVisible(true);
+        
+        
+        this.setBounds(500, 400, 500, 400);
+        this.setDefaultCloseOperation(3);
+        this.setTitle("ramka z rozszerzenia");
+        
+//        this.pack();
+        
     }
    
     public static void main(String[] args)
     {
-     new Ramka();
+     new Ramka().setVisible(true);
     }
     
 }
