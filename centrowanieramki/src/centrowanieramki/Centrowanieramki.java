7c375c19e0f6362b26cb5a86d6afa419ca4fffd1
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-19 14:13:40 +0200

    new task with frame

---
+++ centrowanieramki/src/centrowanieramki/Centrowanieramki.java
@@ -0,0 +1,24 @@
+
+package centrowanieramki;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Centrowanieramki extends JFrame
+{
+    public Centrowanieramki()
+    {
+        
+        
+        this.setDefaultCloseOperation(3);
+    }
+ 
+    public static void main(String[] args)
+    {
+        new Centrowanieramki().setVisible(true);
+        
+        
+    }
+    
+}
