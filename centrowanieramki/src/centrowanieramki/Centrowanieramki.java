84c19b32147cc0adbae5cafd7192b8cb97b37889
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-19 14:28:19 +0200

    second commit
    new task with frame

--- centrowanieramki/src/centrowanieramki/Centrowanieramki.java
+++ centrowanieramki/src/centrowanieramki/Centrowanieramki.java
@@ -9,7 +9,22 @@ public class Centrowanieramki extends JFrame
 {
     public Centrowanieramki()
     {
+        int szerokosc = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int wysokosc = Toolkit.getDefaultToolkit().getScreenSize().height;
         
+        System.out.println(szerokosc);
+        System.out.println(wysokosc);
+        
+        this.setSize(szerokosc/2,wysokosc/2);
+        
+//        this.setLocation(szerokosc/2,400);
+        
+
+        int szerokoscRamki = this.getSize().width;
+        int wysokoscRamki = this.getSize().height;
+        
+        
+        this.setLocation((szerokosc-szerokoscRamki)/2,(wysokosc-wysokoscRamki)/2); // zawsze wysrodkuje ramke
         
         this.setDefaultCloseOperation(3);
     }
