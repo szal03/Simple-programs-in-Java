ef8e791f26ec4033e53057f16bdcbe8ae5a646ac
szal03 <33811775+szal03@users.noreply.github.com>
2018-07-31 14:10:03 +0200

    second commit

--- ukladgraficzny/src/ukladgraficzny/Ukladgraficzny.java
+++ ukladgraficzny/src/ukladgraficzny/Ukladgraficzny.java
@@ -1,11 +1,37 @@
 
 package ukladgraficzny;
 
-public class Ukladgraficzny {
+import javax.swing.*;
+import java.awt.*;
 
-
+public class Ukladgraficzny extends JFrame
+{
+    
+    public Ukladgraficzny()
+    {
+        super("Układ graficzny(Layout)");
+        this.setBounds(300, 320, 300, 200);
+        
+        initComponents();
+        
+        this.setDefaultCloseOperation(3);
+    }
+    
+    public void initComponents()
+    {
+        przycisk1 = new JButton("Hej!");
+        
+        Container kontener = this.getContentPane();
+        
+        kontener.add(przycisk1);
+    }
+    
+    JButton przycisk1;
+    
     public static void main(String[] args) 
     {
+        new Ukladgraficzny().setVisible(true);
+        
         
     }
     
