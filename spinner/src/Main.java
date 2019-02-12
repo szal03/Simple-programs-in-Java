45660c8625f3dba7398eef194a6dc9bf2900f2a0
szal03 <33811775+szal03@users.noreply.github.com>
2019-02-12 17:53:57 +0100

    3 spinnery
    Spiner z miesiącami, datą i liczbami

--- spinner/src/Main.java
+++ spinner/src/Main.java
@@ -1,21 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DateFormatSymbols;
 
 
 public class Main extends JFrame{
     public Main()
     {
-
+        initComponents();
     }
     public void initComponents()
     {
         this.setTitle("Spinners");
-        this.setBounds(300,300,300,200);
+        this.setBounds(500,500,500,500);
+
+        String[] miesiace= new DateFormatSymbols().getMonths();
 
 
 
+        SpinnerListModel modelMiesiecy = new SpinnerListModel(miesiace);
 
+        JSpinner spinner = new JSpinner(modelMiesiecy);
+       // spinner.setEditor(new JTextArea("lalal"));
+
+
+        SpinnerNumberModel modelLiczb = new SpinnerNumberModel(1,1,5,2);
+
+        JSpinner spinner2 = new JSpinner(modelLiczb);
+
+        
+
+        SpinnerDateModel modelDat = new SpinnerDateModel();
+        JSpinner spinner3 = new JSpinner(modelDat);
+
+        JPanel panel = new JPanel();
+        panel.add(spinner);
+        panel.add(spinner2);
+        panel.add(spinner3);
+        this.getContentPane().add(panel,BorderLayout.NORTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args)
