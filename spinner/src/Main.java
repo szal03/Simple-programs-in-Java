8d03e8b1d02a1be8b02d97376ed2f5efd5781a3b
szal03 <33811775+szal03@users.noreply.github.com>
2019-02-12 18:36:23 +0100

    koniec pracy

--- spinner/src/Main.java
+++ spinner/src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Main extends JFrame{
@@ -18,7 +20,7 @@ public class Main extends JFrame{
 
 
 
-        SpinnerListModel modelMiesiecy = new SpinnerListModel(miesiace);
+        MySpinnerListModel modelMiesiecy = new MySpinnerListModel(obetnijArray(miesiace,0,11));
 
         JSpinner spinner = new JSpinner(modelMiesiecy);
        // spinner.setEditor(new JTextArea("lalal"));
@@ -28,9 +30,15 @@ public class Main extends JFrame{
 
         JSpinner spinner2 = new JSpinner(modelLiczb);
 
-        
+        Calendar kalendarz = Calendar.getInstance();
 
-        SpinnerDateModel modelDat = new SpinnerDateModel();
+        Date poczatkowaWartosc = kalendarz.getTime();
+        kalendarz.add(Calendar.DAY_OF_MONTH,-20);
+        Date minimalnaWartosc = kalendarz.getTime();
+        kalendarz.add(Calendar.DAY_OF_MONTH,40);
+        Date maxymalnaWartosc = kalendarz.getTime();
+
+        SpinnerDateModel modelDat = new SpinnerDateModel(poczatkowaWartosc,minimalnaWartosc,maxymalnaWartosc, Calendar.DAY_OF_MONTH);
         JSpinner spinner3 = new JSpinner(modelDat);
 
         JPanel panel = new JPanel();
@@ -39,6 +47,55 @@ public class Main extends JFrame{
         panel.add(spinner3);
         this.getContentPane().add(panel,BorderLayout.NORTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    private class MySpinnerListModel extends SpinnerListModel{
+        public MySpinnerListModel(Object[] values)
+        {
+            super(values);
+            firstValue=values[0];
+            lastValue=values[values.length-1];
+        }
+        @Override
+        public Object getNextValue()
+        {
+            if(super.getNextValue()==null)
+            {
+                return firstValue;
+            }
+            else {
+                return super.getNextValue();
+            }
+        }
+        @Override
+        public Object getPreviousValue()
+        {
+            if(super.getPreviousValue()==null)
+            {
+                return lastValue;
+            }
+            else {
+                return super.getPreviousValue();
+            }
+        }
+        Object firstValue, lastValue;
+    }
+    /**
+     * obcina tablice od <code>poczatek</code> do <code>koniec</code>
+     * @param operowany tablica ktora będzie operowana
+     * @param poczatek indeks od kąd zaczac obcinanie
+     * @param koniec indeks gdzie zakończyć obcinanie
+     * @return zwraca obciętą tablicę
+     * */
+    private Object[]obetnijArray(Object[] operowany, int poczatek, int koniec)
+    {
+        koniec = koniec > operowany.length-1 || koniec<0 ? operowany.length-1:koniec;
+        poczatek=poczatek<0 || poczatek>koniec ? 0:poczatek;
+       Object[] tmp = new Object[koniec+1-poczatek];
+       for(int i=poczatek,j=0;i<koniec+1; i++, j++)
+       {
+           tmp[j] = operowany[i];
+       }
+        return tmp;
     }
     public static void main(String[] args)
     {
