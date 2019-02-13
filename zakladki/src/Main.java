5fa7995d1002b20e3b4737d25256008314515934
szal03 <33811775+szal03@users.noreply.github.com>
2019-02-13 11:05:26 +0100

    Zakladki
    Stworzenie zakładek z różnymi elementami

--- zakladki/src/Main.java
+++ zakladki/src/Main.java
@@ -13,7 +13,30 @@ public class Main extends JFrame{
         this.setTitle("Zakładki");
         this.setBounds(300,300,300,300);
 
+        JPanel panel = new JPanel();
+        panel.add(new JButton("test"));
+
+        zakladki.addTab("tab1",new JLabel("to jest pierwszy"));
+        zakladki.setMnemonicAt(0,KeyEvent.VK_T); //alt+t -> przeniesie do pierwszej zakladki
+
+
+        zakladki.addTab("to jest drugi tab",new ImageIcon("ikonka.jpg"), panel,"tip");
+
+        zakladki.addTab("zakladka3",panelTworzenia);
+        panelTworzenia.add(new JLabel("Stwórz plik o nazwie: "));
+        JTextField nazwaNowejZakladki = new JTextField(7);
+        panelTworzenia.add(nazwaNowejZakladki);
+        JButton stworzZakladke = new JButton("Stwórz");
+        panelTworzenia.add(stworzZakladke);
+
+        this.getContentPane().add(zakladki);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+    private JTabbedPane zakladki = new JTabbedPane();
+
+    private JPanel panelTworzenia = new JPanel();
+
     public static void main(String[] args)
     {
         new Main().setVisible(true);
