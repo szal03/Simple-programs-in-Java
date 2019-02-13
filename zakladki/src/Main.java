1ef0eec126b05ca65da137d8c2262024cea218e6
szal03 <33811775+szal03@users.noreply.github.com>
2019-02-13 11:55:59 +0100

    koniec pracy

--- zakladki/src/Main.java
+++ zakladki/src/Main.java
@@ -24,11 +24,59 @@ public class Main extends JFrame{
 
         zakladki.addTab("zakladka3",panelTworzenia);
         panelTworzenia.add(new JLabel("Stwórz plik o nazwie: "));
-        JTextField nazwaNowejZakladki = new JTextField(7);
+        final JTextField nazwaNowejZakladki = new JTextField(7);
         panelTworzenia.add(nazwaNowejZakladki);
         JButton stworzZakladke = new JButton("Stwórz");
         panelTworzenia.add(stworzZakladke);
 
+        stworzZakladke.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                JPanel panelTextowy = new JPanel();
+                panelTextowy.setLayout(new GridLayout(1,1));
+                JTextArea obszarTekstowy = new JTextArea();
+                panelTextowy.add(new JScrollPane(obszarTekstowy));
+                zakladki.addTab(nazwaNowejZakladki.getText()+".txt",panelTextowy);
+                zakladki.setSelectedIndex(zakladki.getTabCount()-1);
+
+                obszarTekstowy.addKeyListener(new KeyAdapter() {
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                        if(!(tytulZakladki+e.getKeyChar()).equals(przedZmianaObszaruTekstowego) && czyToAscii(e.getKeyChar()) && flagaZapisu)
+                        {
+                            przedZmianaObszaruTekstowego=tytulZakladki+e.getKeyChar();
+                            zakladki.setTitleAt(indexZakladki,tytulZakladki+"*");
+                            flagaZapisu = false;
+                        }
+                        else if(e.isControlDown() && e.getKeyCode() == KeyEvent.VK_S && !flagaZapisu)
+                        {
+                            zakladki.setTitleAt(indexZakladki, tytulZakladki);
+                            System.out.println("Zapisujemy");
+                            flagaZapisu = true;
+                        }
+
+                    }
+                    private boolean czyToAscii(char zn)
+                    {
+                        for(int i=0; i<255; i++)
+                        {
+                            if(zn==i)
+                            {
+                                return true;
+                            }
+
+                        }
+                        return false;
+                    }
+                    String przedZmianaObszaruTekstowego="";
+                    int indexZakladki = zakladki.getSelectedIndex();
+                    String tytulZakladki = zakladki.getTitleAt(indexZakladki);
+                    boolean flagaZapisu = true;
+                });
+            }
+        });
+        zakladki.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
         this.getContentPane().add(zakladki);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
