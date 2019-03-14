484de0110d0ee472113c24ddefe805620e1116ca
szal03 <33811775+szal03@users.noreply.github.com>
2019-03-14 10:39:43 +0100

    Implementacja buttonów
    Implementacja buttonu "dodaj".

--- ZipperInterface/src/Main.java
+++ ZipperInterface/src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Main extends JFrame {
 
@@ -26,6 +28,8 @@ public class Main extends JFrame {
         bUsun = new JButton(akcjaUsuwania);
         bZip = new JButton(akcjaZipowania);
 
+        JScrollPane scrollek = new JScrollPane(lista);
+
         lista.setBorder(BorderFactory.createEtchedBorder());
         GroupLayout layout = new GroupLayout(this.getContentPane());
 
@@ -33,13 +37,13 @@ public class Main extends JFrame {
         layout.setAutoCreateGaps(true);
         layout.setHorizontalGroup(
                 layout.createSequentialGroup()
-                        .addComponent(lista, 100, 150, Short.MAX_VALUE)
+                        .addComponent(scrollek, 100, 150, Short.MAX_VALUE)
                         .addContainerGap(0,Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup().addComponent(bDodaj).addComponent(bUsun).addComponent(bZip))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup()
-                        .addComponent(lista, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(scrollek, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createSequentialGroup().addComponent(bDodaj).addComponent(bUsun).addGap(5,40, Short.MAX_VALUE).addComponent(bZip)));
         this.getContentPane().setLayout(layout);
 
@@ -47,18 +51,32 @@ public class Main extends JFrame {
 
         this.pack();
     }
-    private JList lista = new JList();
+    private DefaultListModel modelListy = new DefaultListModel()
+    {
+        @Override
+        public void addElement(Object obj)
+        {
+            lista.add(obj);
+           super.addElement(((File)obj).getName());
+        }
+        @Override
+        public Object get(int index)
+        {
+            return lista.get(index);
+        }
+        ArrayList lista = new ArrayList();
+    };
+    private JList lista = new JList(modelListy);
     private  JButton bDodaj;
     private  JButton bUsun;
     private  JButton bZip;
     private JMenuBar pasekMenu = new JMenuBar();
-
-
-
+    private JFileChooser wybieracz = new JFileChooser();
 
 
     public static void main(String[] args)
     {
+
         new Main().setVisible(true);
 
     }
@@ -83,11 +101,44 @@ public class Main extends JFrame {
         public void actionPerformed(ActionEvent e)
         {
            if(e.getActionCommand().equals("Dodaj"))
-               System.out.println("Dodawanie");
+              // System.out.println("Dodawanie");
+               dodajWpisyDOArchwium();
            else if(e.getActionCommand().equals("Usuń"))
                System.out.println(("Usuwanie"));
            else if(e.getActionCommand().equals("Zip"))
                System.out.println("Zippowanie");
         }
+
+    }
+    private void dodajWpisyDOArchwium()
+    {
+        wybieracz.setCurrentDirectory(new File(System.getProperty("user.dir"))); //skąd zaczyna szukanie
+        wybieracz.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES); //mozna wybierac pliki i katalogi
+        wybieracz.setMultiSelectionEnabled(true); // aby można było wybrać wiele plików
+        int tmp = wybieracz.showDialog(rootPane, "Dodaj do archiwum");
+        if(tmp == JFileChooser.APPROVE_OPTION) //opcja akceptująca w JFileChooser
+        {
+            File[] sciezki = wybieracz.getSelectedFiles(); // pobiera wybrane pliki
+
+            for(int i=0; i<sciezki.length; i++)
+            {
+                if(!czyWpisSiePowtarza(sciezki[i].getPath()))
+                modelListy.addElement(sciezki[i]);
+            }
+
+        }
+
+
+    }
+    //sprawdzamy czy nie dodano dwa razy
+    private boolean czyWpisSiePowtarza(String testowanyWpis)
+    {
+        for(int i =0; i<modelListy.getSize(); i++)
+        {
+            if(((File)modelListy.get(i)).getPath().equals(testowanyWpis))
+                return true;
+        }
+
+        return false;
     }
 }
